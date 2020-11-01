992a328a3c0bb7bea9ecae418384cb243c27ecd8
Crowdbotics <team@crowdbotics.com>
2020-11-01 16:34:03 +0000

    add package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'UserProfile166202', human_name: 'User Profile', access_route: 'UserProfile166202'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
